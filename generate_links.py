8a4620c56a5dca17a27967a2f749a935a2ad39d6
Aymen Hmani <aymen.hmani.tech@gmail.com>
2025-05-09 11:28:47 +0100

    Add README file update functionality and improve index.html metadata

--- generate_links.py
+++ generate_links.py
@@ -3,6 +3,7 @@ import time
 
 BASE_URL = "https://sou9-nft.github.io/flutter-flow-assets/"
 INDEX_FILE = "index.html"
+README_FILE = "redme.md"  # Note the filename as seen in the workspace
 ASSETS_FOLDER = "assets"
 
 # Generate links for all assets in the assets folder
@@ -45,6 +46,22 @@ def write_index_file(links):
         f.write("</body>\n")
         f.write("</html>\n")
 
+# Update the README.md file with the index link as a title
+
+
+def update_readme_file():
+    timestamp = int(time.time())
+    index_url = f"{BASE_URL}{INDEX_FILE}?v={timestamp}"
+
+    with open(README_FILE, "w") as f:
+        f.write(f"# [Asset Links]({index_url})\n\n")
+        f.write("This repository contains assets used for Flutter Flow projects.\n")
+        f.write(
+            f"Last updated: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}\n\n")
+        f.write("## Available Assets\n\n")
+        f.write(
+            f"View all available assets at the [Asset Links Index]({index_url}).\n")
+
 # Clear the list section in the index.html file
 
 
@@ -77,3 +94,4 @@ if __name__ == "__main__":
     clear_index_file()
     links = generate_links()
     write_index_file(links)
+    update_readme_file()  # Added the call to update the README file
---
+++ redme.md
@@ -0,0 +1,8 @@
+# [Asset Links](https://sou9-nft.github.io/flutter-flow-assets/index.html?v=1746786492)
+
+This repository contains assets used for Flutter Flow projects.
+Last updated: 2025-05-09 11:28:12
+
+## Available Assets
+
+View all available assets at the [Asset Links Index](https://sou9-nft.github.io/flutter-flow-assets/index.html?v=1746786492).
